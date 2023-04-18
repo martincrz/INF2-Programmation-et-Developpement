9b422bdf4dfbf580f8b96a923ff6ba6089d4a216
martincrz <115504744+martincrz@users.noreply.github.com>
2023-04-18 21:27:39 +0200

    Create ex4.py

---
+++ TD1/ex4.py
@@ -0,0 +1,24 @@
+text=input("Entrez un texte : ")
+print(text)
+print("Le nombre de caractères de la liste est de ", len(text))
+
+#nb de lettres
+
+#commande
+for carac in text
+    if carac.isdigit():       #si c est un nb
+        nb_nbre=nb_nbre+1
+#islower() si c est une minuscul et   is upper() si c'est une maj
+    if ord(carac)>=ord("a") and ord(carac)
+ou carac=['a','b','c','d'...]
+if caract.lower() in list_voyelle
+
+nb_voyelles= text.count("a")+text.count("e")+text.count("i")+text.count("o")+text.count("u")+text.count("y")
+nb_chiffres= text.count("0")+text.count("1")+text.count("2")+text.count("3")+text.count("4")+text.count("5")+text.count("6")+text.count("7")+text.count("8")+text.count("9")
+print("Il y a ", nb_voyelles, " voyelles.")
+print("Il y a ", nb_chiffres, " chiffres")
+
+#dictionnaire, notion clé et valeur
+#création
+mon_dict={}
+
